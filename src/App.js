d7161fc5c79d6bbe14771e0fd3b25cd78c14f06b
Buğra Aydın <bugraaydin1@hotmail.com>
2022-02-15 00:21:48 +0300

    Change DoneList comp route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import AppBar from "./components/AppBar";
+import DoneList from "./components/DoneList";
 import TodoList from "./components/TodoList";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 			<AppBar />
 			<Routes>
 				<Route path="/todo" element={<TodoList />} />
-				<Route path="/done" element={<h1>Done</h1>} />
+				<Route path="/done" element={<DoneList />} />
 				<Route path="/" element={<Navigate to="/todo" />} />
 			</Routes>
 		</>
